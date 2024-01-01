519f4b0a0326c3435b2f0236aef760f4e8337e1a
TheNochtgamer <61437465+TheNochtgamer@users.noreply.github.com>
2024-01-01 07:42:42 -0300

    test

--- modem2.lua
+++ modem2.lua
@@ -1,4 +1,6 @@
 local splitFn = require "splitFn"
+local expect = require "cc.expect"
+local expect, field = expect.expect, expect.field
 
 local modem = peripheral.find("modem") or error("No modem attached", 0)
 modem.open(99)
@@ -16,6 +18,9 @@ repeat
     local args = splitFn(message, " ")
 
     if args[0] == "redstone" then
+        expect(1, args[1], "back", "front", nil)
+        expect(2, args[2], "string", nil)
+
         redstone.setAnalogOutput(args[1] or "back", tonumber(args[2] or 1))
     end
 until message == "stop"
