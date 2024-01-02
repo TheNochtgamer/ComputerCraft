eef93aee7ece2b8194d5a3c674566ed857e3686e
TheNochtgamer <61437465+TheNochtgamer@users.noreply.github.com>
2024-01-02 04:02:55 -0300

    fix

--- autoDownload.lua
+++ autoDownload.lua
@@ -3,18 +3,19 @@ local expect = require "cc.expect"
 expect(1, arg[1], "string")
 expect(2, arg[2], "string", "nil")
 
-local scriptName = "auto-" .. (arg[2] or "script.lua")
+local targetScript = arg[1]:match("([^/]+)$")
+local scriptName = arg[2] or ("auto-script.lua")
 
 if fs.exists(scriptName) then
     fs.delete(scriptName)
 end
 
 local file = fs.open(scriptName, "w");
-file.write("fs.delete(\"" .. scriptName .. "\")\n")
+file.write("fs.delete(\"" .. targetScript .. "\")\n")
 file.write("shell.run(\"wget\", \"" ..
-    arg[1] .. "\", \"" .. scriptName .. "\")\n")
-file.write("print(\"running\", \"" .. scriptName .. "\")")
-file.write("shell.run(\"" .. scriptName .. "\")\n")
+    arg[1] .. "\", \"" .. targetScript .. "\")\n")
+file.write("print(\"running\", \"" .. targetScript .. "\")")
+file.write("shell.run(\"" .. targetScript .. "\")\n")
 file.close()
 
 
