7981b60dec7fbc81af8dde5cb21d623526028a64
TheNochtgamer <61437465+TheNochtgamer@users.noreply.github.com>
2024-01-13 06:00:28 -0300

    reworked

--- autoPacker.lua
+++ autoPacker.lua
@@ -1,68 +1,36 @@
 -- Config
 
-local outputItem = ""
+-- local outputItem = ""
 
 -- Functions
 
 function Main()
     while true do
+        local offset = 0
+
         for i = 1, 9, 1 do
-            if turtle.getItemCount(i) == 0 then
+            if i == 4 then offset = 1 end
+            if i == 7 then offset = 2 end
+            local pos = i + offset
+
+            if turtle.getItemCount(pos) == 0 then
+                turtle.select(pos)
                 if not turtle.suck() then
                     sleep(20)
                 end
             end
         end
 
-        if turtle.getItemDetail(4) then
-            turtle.select(4)
-            turtle.transferTo(10)
-            turtle.select(1)
-        end
-
-        if turtle.getItemDetail(8) then
-            turtle.select(8)
-            turtle.transferTo(11)
-            turtle.select(1)
-        end
-
-        local function dropExtra()
-            if turtle.getItemCount(4) > 0 then
-                turtle.select(4)
-                turtle.drop()
-                turtle.select(1)
-            end
-
-            if turtle.getItemCount(8) > 0 then
-                turtle.select(8)
-                turtle.drop()
-                turtle.select(1)
-            end
-
-            for i = 12, 16, 1 do
-                if turtle.getItemCount(i) > 0 then
-                    turtle.select(i)
-                    turtle.drop()
-                    turtle.select(1)
-                end
-            end
-        end
-
-
+        turtle.select(16)
         if not turtle.craft() then
-            dropExtra()
-            turtle.craft()
+            for i = 1, 16, 1 do
+                turtle.select(i)
+                turtle.drop()
+            end
+            break
         end
 
-        for i = 1, 16, 1 do
-            turtle.select(i)
-            if turtle.getItemDetail(i) then
-                if turtle.getItemDetail(i).name == outputItem then
-                    turtle.dropDown()
-                    break
-                end
-            end
-        end
+        turtle.dropDown()
     end
 end
 
