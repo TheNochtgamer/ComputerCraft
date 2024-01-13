2c982fe23a4aed81abec6c2c0744c78af813acf1
TheNochtgamer <61437465+TheNochtgamer@users.noreply.github.com>
2024-01-13 05:39:30 -0300

    fix

--- autoPacker.lua
+++ autoPacker.lua
@@ -1,16 +1,39 @@
-while true do
-    sleep(0.1)
-    for i = 1, 9 do
-        turtle.suck()
+-- Config
+
+local outputItem = ""
+
+-- Functions
+
+function Main()
+    while true do
+        for i = 1, 9, 1 do
+            if not turtle.suck() then
+                sleep(20)
+            end
+        end
+
+        if turtle.getItemDetail(4) then
+            turtle.select(4)
+            turtle.transferTo(10)
+            turtle.select(1)
+        end
+
+        if turtle.getItemDetail(8) then
+            turtle.select(8)
+            turtle.transferTo(11)
+            turtle.select(1)
+        end
+
+        turtle.craft()
+
+        for i = 1, 16, 1 do
+            turtle.select(i)
+            if turtle.getItemDetail(i) then
+                if turtle.getItemDetail(i).name == outputItem then
+                    turtle.dropDown()
+                    break
+                end
+            end
+        end
     end
-
-    turtle.select(4)
-    turtle.transferTo(11)
-    turtle.select(8)
-    turtle.transferTo(12)
-
-    turtle.craft()
-
-    turtle.select(1)
-    turtle.dropDown()
 end
