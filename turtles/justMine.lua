1acd05bed81fba9ad1f605691c9aaea5132670ff
TheNochtgamer <61437465+TheNochtgamer@users.noreply.github.com>
2024-04-30 16:49:59 -0300

    Corregidos algunos errores en justMine.lua

--- turtles/justMine.lua
+++ turtles/justMine.lua
@@ -5,7 +5,8 @@ local modem = peripheral.find("modem") or error("Requires a modem to run")
 
 -- Configs
 
-local listeningChannel = 4041;
+local _debug = false
+local listeningChannel = 23;
 local mineDirection = "forward";
 
 -- Vars
@@ -13,7 +14,7 @@ local mineDirection = "forward";
 settings.define("isActive", {
   description = "Determina si el bot estaba funcionando o no",
   type = "boolean",
-  default = false
+  default = true
 })
 
 -- Functions
@@ -24,13 +25,10 @@ local function miningLoop()
     if (settings.get("isActive")) then
       if (mineDirection == "forward") then
         turtle.dig()
-        turtle.forward()
       elseif (mineDirection == "up") then
         turtle.digUp()
-        turtle.up()
       elseif (mineDirection == "down") then
         turtle.digDown()
-        turtle.down()
       end
     end
   end
@@ -43,9 +41,13 @@ local function messageSwitch()
     if (message:match("on." .. os.getComputerID()) or message:match("on.all")) then
       settings.set("isActive", true)
       print("Bot iniciado, por mensaje remoto")
+
+      modem.send(replyChannel, listeningChannel, "Bot online")
     elseif (message:match("off." .. os.getComputerID()) or message:match("off.all")) then
       settings.set("isActive", false)
       print("Bot detenido, por mensaje remoto")
+
+      modem.send(replyChannel, listeningChannel, "Bot offline")
     end
     settings.save("justMine.cfg")
   end
@@ -60,9 +62,21 @@ function Main()
   modem.open(listeningChannel)
 
   term.clear()
+  term.setCursorPos(1, 1)
+
   print("JustMine iniciado")
+  print("Escuchando en canal " .. listeningChannel)
+  print("Estado: " .. (settings.get("isActive") and "activo" or "inactivo"))
+  print("")
+
+  local debug_loops = 0;
   while true do
     parallel.waitForAny(miningLoop, messageSwitch)
+
+    debug_loops = debug_loops + 1
+    if (_debug) then
+      print("loop " .. debug_loops)
+    end
   end
 end
 Main();
